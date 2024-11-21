8995b8a3c7d940ba471183dd1de7b536cccf02dd
jyoey <jzhang.joe03@gmail.com>
2024-11-21 13:09:45 -0500

    transaction popup template

---
+++ Frontend/rewards-app/src/view/TransactionPopup.js
@@ -0,0 +1,37 @@
+import React from "react";
+import './TransactionPopup.css'
+
+function TransactionPopup({ reward, isOpen, onClose}) {
+    if (!isOpen) return null;
+
+    return (
+        <div className="transac-overlay">
+            <div className="transac-content">
+            <button className="transac-close-button" onClick={onClose}>X</button>
+                <div className="left-col">
+                    <h2 className="reward-title">{reward.title}</h2>
+                    <h4 className="reward-exp-date">Thank you for your reward redemption!</h4>
+                    <div className="textbox">
+                        <p className="description-text">Description</p>
+                        <br></br>
+                        <p className="terms-text">Terms and conditions</p>
+                    </div>
+                </div>
+                <div className="right-col">
+                    <div className="qr-code-box">
+                        {/* qr code */}
+                        <div className="qr-code-placeholder">
+                            QR Code
+                        </div>
+                    </div>
+                    <div className="buttons-container">
+                        <button className="email-button" >Send To Email</button>
+                        <button className="download-button" >Download As PDF</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default TransactionPopup
