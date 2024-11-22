a877cb421257dbae7e50dcf79b2ba9cb02e25f56
jyoey <jzhang.joe03@gmail.com>
2024-11-22 10:55:27 -0500

    adjusted design with item components

--- Frontend/rewards-app/src/view/Transactions.js
+++ Frontend/rewards-app/src/view/Transactions.js
@@ -4,21 +4,19 @@ import './RewardDetails.css';
 
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-import TransactionPopup from './TransactionPopup';
+import TransactionCard from '../components/TransactionCard';
 
-function Transactions() {
-    const { activetransaction_id } = useParams(); // Corrected typo here
-    const { inactivetransaction_id } = useParams(); // Corrected typo here
+function Transactions({user}) {
+    const { activetransaction_id } = useParams();
+    const { inactivetransaction_id } = useParams();
     const [activeTransactions, setActiveTransactions] = useState([]);
     const [inactiveTransactions, setInactiveTransactions] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const [showTransactionPopup, setShowTransactionPopup] = useState(false);
 
-    const user_id = 1;
-
     useEffect(() => {
-        fetch('http://localhost:8000/lprs/rewardbyuser/1/' + user_id)
+        fetch('http://localhost:8000/lprs/rewardbyuser/1/' + user.user_id)
             .then(response => {
                 if (!response.ok) {
                     console.error("Response Status:", response.status);
@@ -39,7 +37,7 @@ function Transactions() {
     }, [activetransaction_id]);
 
     useEffect(() => {
-        fetch('http://localhost:8000/lprs/rewardbyuser/0/' + user_id)
+        fetch('http://localhost:8000/lprs/rewardbyuser/0/' + user.user_id)
             .then(response => {
                 if (!response.ok) {
                     console.error("Response Status:", response.status);
@@ -57,10 +55,10 @@ function Transactions() {
                 setError(error.message);
                 setLoading(false);
             });
-    }, [inactivetransaction_id]);
+    }, [user, inactivetransaction_id]);
 
     if (loading) {
-        return <div>Loading...</div>;
+        console.log('loading');
     }
 
     if (error) {
@@ -70,44 +68,27 @@ function Transactions() {
     return (
         <div class="reward-transactions-container">
             <h1>Reward Transactions</h1>
+            <h2>Active</h2>
+            <div className='active-transaction-container'>
+                {activeTransactions.length > 0 ? (
+                    activeTransactions.map(transaction => (
+                        <TransactionCard key={transaction.transaction_id} transaction={transaction} active={true}/>
+                    ))
+                ) : (
+                    console.log('No active transactions for this user')
+                )}
+            </div>
             <br></br>
-            <div>
-                <h2>Active</h2>
-                <div>
-                    {activeTransactions.map((transaction) => (
-                        <div class="each-transaction" key={transaction.transaction_id}>
-                            <img src={transaction.image} alt="Reward Image" />
-                                <div>
-                                    <h3>{transaction.title}</h3>
-                                    <p>End date:Ongoing</p>
-                                </div>
-                            <button type="button" onClick={() => setShowTransactionPopup(true)}>Redeem</button>
-                        </div>
-                    ))}
-                </div>
+            <h2>Inactive</h2>
+            <div className='inactive-transaction-container'>
+                {inactiveTransactions.length > 0 ? (
+                    inactiveTransactions.map(transaction => (
+                        <TransactionCard key={transaction.transaction_id} transaction={transaction}/>
+                    ))
+                ) : (
+                    console.log('No inactive transactions for this user')
+                )}
             </div>
-            <div>
-                <h2>Inactive</h2>
-                <div>
-                    {inactiveTransactions.map((transaction) => (
-                        <div class="each-transaction" key={transaction.transaction_id}>
-                            <img src={transaction.image} alt="Reward Image" />
-                            <div>
-                                <h3>{transaction.title}</h3>
-                                <p>End date:Ongoing</p>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-            </div>
-
-            {showTransactionPopup && (
-                <TransactionPopup
-                    reward={activeTransactions}
-                    isOpen={showTransactionPopup}
-                    onClose={() => setShowTransactionPopup(false)}
-                />
-            )}
         </div>
     );
 }
