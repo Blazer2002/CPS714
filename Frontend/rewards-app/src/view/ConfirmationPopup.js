13d4792082e002bb6bf72cff559fed15bbd74970
jyoey <jzhang.joe03@gmail.com>
2024-11-22 10:53:42 -0500

    update

--- Frontend/rewards-app/src/view/ConfirmationPopup.js
+++ Frontend/rewards-app/src/view/ConfirmationPopup.js
@@ -2,8 +2,7 @@ import React from "react";
 import './ConfirmationPopup.css';
 import formatDate from '../utility.js';
 
-function ConfirmationPopup({ reward, isOpen, onClose, onConfirm}) {
-    {/* get user */}
+function ConfirmationPopup({ user, reward, isOpen, onClose, onConfirm}) {
     if (!isOpen) return null;
 
     let startDateFormatted = formatDate(reward.start);
@@ -18,20 +17,20 @@ function ConfirmationPopup({ reward, isOpen, onClose, onConfirm}) {
                 </div>
                 <div className="points-info">
                     <h3>Redemption Amount: {reward.points}</h3>
-                    Current Points: {/* user.points */}
+                    Current Points: {user.points}
                 </div>
                 <div className="info-container">
                     <span className="align-left">
-                        <h4>Product Name</h4>
+                        <h4>Reward Name</h4>
                         <h4>Reward Start Date</h4>
                         <h4>Product Expiration Date</h4>
                         <h4>Points After Redemption</h4>
                     </span>
                     <span className="align-right">
-                        <h4>{reward.title}</h4>
+                        <h4>{reward.rewardname}</h4>
                         <h4>{startDateFormatted}</h4>
                         <h4>{endDateFormatted}</h4>
-                        <h4>{/* user.points - reward.points */}Pts</h4>
+                        <h4>{user.points-reward.points} Pts</h4>
                     </span>
                 </div>
                 <br></br>
